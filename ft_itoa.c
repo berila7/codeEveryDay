226f92fb67dd6cec91bb34ae968b202c8c2ef357
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-02-12 09:51:45 +0100

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,13 +5,9 @@ int	number_count(int nbr)
 {
 	int len;
 
-	if (nbr < 0)
-	{
+	len = 0;
+	if (nbr <= 0)
 		len++;
-		nbr *= -1;
-	}
-	if (nbr == 0)
-		return (1);
 	while (nbr)
 	{
 		nbr /= 10;
@@ -23,32 +19,35 @@ int	number_count(int nbr)
 
 char	*ft_itoa(int number)
 {
-	char	*string;
 	int		len;
-	int		tmp_number;
+	char	*string;
+	long	tmp_number;
 
-	tmp_number = number;
 	len = number_count(number);
 	string = malloc(sizeof(char) * len + 1);
+
 	if (!string)
 		return (NULL);
 	string[len] = '\0';
-	if (number == -2147483648)
-	{
-		free(string);
-		return ("-2147483648");
-	}
+
+	tmp_number = number;
 	if (number < 0)
+	{
+		string[0] = '-';
 		tmp_number = - number;
-	while (len >= 0 && tmp_number != 0)
+	}
+
+	if (number == 0)
+	{
+		string[0] = '0';
+		return (string);
+	}
+	while (tmp_number)
 	{
 		len--;
 		string[len] = (tmp_number % 10) + '0';
 		tmp_number /= 10;
 	}
-	if (number < 0)
-		string[0] = '-';
-	if (number == 0)
-		return (free(string), "0");
+	
 	return (string);
 }
